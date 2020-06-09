446b849d5ed29f56de19b6ae9d0f77d376c8cbc5
Christian Cataldo <epost345@gmail.com>
2020-06-09 17:50:59 +0300

    Implement basic HTTP GET request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VAT-check';
+  url = `https://vat.erply.com/numbers?vatNumber=BG999999999`;
+  items = [];
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+      for (let key in data)
+        if (data.hasOwnProperty(key))
+          this.items.push(key, data[key]);
+    });
+  }
 }
